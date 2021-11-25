b71939db9c943a66e23f323eb6ceeb535dc704b5
Andru Zaverukha <zawer0103@gmail.com>
2021-11-25 17:31:04 +0200

    module0 & module1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -7,14 +7,15 @@ function print(text) {
 
 /**
  * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно  //your_string.indexOf('hello') > -1 // -1 если элемент не найден
+ * чтобы функция sayHello работала корректно  
+ * your_string.indexOf('hello') > -1 // -1 если элемент не найден
  * if (! /^[a-zA-Z0-9]+$/.test(foo)) {
     // Validation failed
 }
+https://www.regular-expressions.info/javascript.html
  */   
 function isValid(name) {
- // if (name.indexOf(' ') > -1) {return false;}
-  
+ // для отдельного символа можно еще и так// if (name.indexOf(' ') > -1) {return false;}  
   if ( ! /^[a-zA-Z0-9]+$/.test(name) || name==null || name.length<4 ) {return false;}
   else {return true;}
 }
