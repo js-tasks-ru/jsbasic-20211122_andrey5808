ef538e8fea17707ac891beecb5e453fd56805922
Andru Zaverukha <zawer0103@gmail.com>
2021-12-07 11:54:35 +0200

    module3 complete

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,45 @@
 function camelize(str) {
-  // ваш код...
+ return  arr = str
+    .split('-')
+    .map( (word, index) => { if(index==0) {return word;
+                            } else { return word.charAt(0).toUpperCase() + word.slice(1);} } )
+    .join('');
+  //console.log(arr);
 }
+
+//camelize('background-color-solor-dolor')=='backgroundColorSolorDolor';
+//camelize('list-style-image') == 'listStyleImage';
+//camelize('-webkit-transition') == 'WebkitTransition';
+
+
+/*
+function camelize2(str) {
+return  arr = str
+    .split('-')
+    .map( (word, index) => index==0? word : word.charAt(0).toUpperCase() + word.slice(1) )
+    .join('');
+  console.log(arr);
+}
+
+camelize2('background-color-solor-dolor')=='backgroundColorSolorDolor';
+camelize2('list-style-image') == 'listStyleImage';
+camelize2('-webkit-transition') == 'WebkitTransition';
+*/
+/*
+function camelize3(str) {
+  let arr = [];
+  arr = str.split('-');
+  console.log(arr);
+  let itemUp = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    itemUp = itemUp + arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    // console.log(itemUp);
+  }
+  console.log(itemUp);
+  return itemUp;
+}
+
+camelize3('background-color-solor-dolor') == 'backgroundColorSolorDolor';
+camelize3('list-style-image') == 'listStyleImage';
+camelize3('-webkit-transition') == 'WebkitTransition';
+*/
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,41 @@
 function showSalary(users, age) {
-  // ваш код...
+  return users.filter (user=> user.age<=age).map(user=> `${user.name}, ${user.balance}`).join('\n');
 }
+
+/*
+let user1 = {
+  "balance": "$1,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 21,
+  "name": "Golden Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let user2 = {
+  "balance": "$1000.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 46,
+  "name": "silver Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let user3 = {
+  "balance": "$2000.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 30,
+  "name": "cuprum Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+// Массив пользователей
+let users = [user1, user2, user3]
+
+let result = showSalary(users, 30);
+console.log(result);
+*/
