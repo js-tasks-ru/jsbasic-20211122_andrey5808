6e3cbb2a286312f1ed19b58031d1ac41841c1702
Andru Zaverukha <zawer0103@gmail.com>
2021-12-23 10:27:45 +0200

    модуль 5 почему task3 не проходит тест

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
-  // ваш код...
+  //тут єто не нужно т.к. функция в самом html вызываеться 
+  //document.addEventListener('DOMContentLoaded', function() {
+  btn=document.querySelector(".hide-self-button"); 
+  btn.onclick = function() {
+    btn.hidden = true;// console.log ('click');
+  }  
+ // })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,46 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight=document.querySelector('.carousel__arrow_right');
+  const arrowLeft=document.querySelector('.carousel__arrow_left');
+    
+  
+  arrowRight.addEventListener('click', arrowRightClicker);
+  arrowLeft.addEventListener('click', arrowLeftClicker);
+ 
+  const slide=document.querySelector('.carousel__inner') ;  //  .firstElementChild    .children[0]
+
+  const slideWidthR = '-'+slide.offsetWidth+'px';
+  const slideWidthL = slide.offsetWidth+'px';
+ 
+  
+  let slideCounter=1;
+  arrowLeft.style.display == "none"; //по умолчанию левая кнопка должна быть скрыта т.к. мы на первом слайде 
+  //и чего оно не скрывается?!
+
+  function arrowRightClicker() {
+    arrowLeft.style.display = "";
+    
+    if ( slideCounter<4)  {arrowRight.style.display = "";
+    slide.style.transform += `translateX( ${slideWidthR})` ;
+    slideCounter++;
+    console.log ('slideCounter='+slideCounter);
+      } 
+      if ( slideCounter>=4)   {arrowRight.style.display = "none"}; 
+   
+  }  
+ 
+  function arrowLeftClicker() {
+      arrowRight.style.display = "";
+   
+    if ( slideCounter>1)  {arrowLeft.style.display = "";
+    slide.style.transform += `translateX( ${slideWidthL})` ;  
+    slideCounter--;
+    console.log ('slideCounter='+slideCounter);
+      }   
+      if ( slideCounter<=1)   {arrowLeft.style.display = "none"}; 
+   
+
+  }  
+
+
+
 }
